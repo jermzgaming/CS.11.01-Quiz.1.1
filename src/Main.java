b6ed8f5304a8840c8e01c7db54ace9b3da0777fa
n30daneo <neoyeanlaic2027@student.cis.edu.hk>
2024-08-30 12:08:31 +0800

    Neo IDE Quiz Finished

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Neo
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Please enter your first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Please enter your favorite food: ");
+        String favouriteFood = scanner.nextLine();
+        System.out.println("\nFirst name: " + firstName + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
     }
 
 }
